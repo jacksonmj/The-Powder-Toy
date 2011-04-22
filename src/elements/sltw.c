cee9f29197df81920e360dc02971b22c87687bd7
jacksonmj <mj-pt@jacksonmj.co.uk>
2011-04-22 21:59:37 +0100

    Experimental improvement for spark conduction in moving water
    If moving water gets sparked, spark tends to form loops and continue travelling
    forever. Using life diffusion between adjacent water particles seems to reduce this.

--- src/elements/sltw.c
+++ src/elements/sltw.c
@@ -1,7 +1,7 @@
 #include <element.h>
 
 int update_SLTW(UPDATE_FUNC_ARGS) {
-	int r, rx, ry;
+	int r, rx, ry, trade;
 	for (rx=-2; rx<3; rx++)
 		for (ry=-2; ry<3; ry++)
 			if (x+rx>=0 && y+ry>0 && x+rx<XRES && y+ry<YRES && (rx || ry))
@@ -19,5 +19,25 @@ int update_SLTW(UPDATE_FUNC_ARGS) {
 					parts[i].life = 4;
 				}
 			}
+	for (trade=0; trade<4; trade++)
+	{
+		rx = rand()%3-1;
+		ry = rand()%3-1;
+		if (x+rx>=0 && y+ry>0 && x+rx<XRES && y+ry<YRES && (rx || ry))
+		{
+			r = pmap[y+ry][x+rx];
+			if ((r>>8)>=NPART || !r)
+				continue;
+			if ((r&0xFF)==PT_SLTW&&(parts[i].life>parts[r>>8].life)&&parts[i].life>0&&parts_avg(r>>8, i, PT_INSL)!=PT_INSL)//diffusion
+			{
+				int temp = parts[i].life - parts[r>>8].life;
+				if (temp>5)
+				{
+					parts[r>>8].life += temp/2;
+					parts[i].life -= temp/2;
+				}
+			}
+		}
+	}
 	return 0;
 }
--- src/elements/watr.c
+++ src/elements/watr.c
@@ -1,7 +1,7 @@
 #include <element.h>
 
 int update_WATR(UPDATE_FUNC_ARGS) {
-	int r, rx, ry;
+	int r, rx, ry, trade;
 	for (rx=-2; rx<3; rx++)
 		for (ry=-2; ry<3; ry++)
 			if (x+rx>=0 && y+ry>0 && x+rx<XRES && y+ry<YRES && (rx || ry))
@@ -25,5 +25,25 @@ int update_WATR(UPDATE_FUNC_ARGS) {
 					kill_part(r>>8);
 				}
 			}
+	for (trade=0; trade<4; trade++)
+	{
+		rx = rand()%3-1;
+		ry = rand()%3-1;
+		if (x+rx>=0 && y+ry>0 && x+rx<XRES && y+ry<YRES && (rx || ry))
+		{
+			r = pmap[y+ry][x+rx];
+			if ((r>>8)>=NPART || !r)
+				continue;
+			if ((r&0xFF)==PT_WATR&&(parts[i].life>parts[r>>8].life)&&parts[i].life>0&&parts_avg(r>>8, i, PT_INSL)!=PT_INSL)//diffusion
+			{
+				int temp = parts[i].life - parts[r>>8].life;
+				if (temp>6)
+				{
+					parts[r>>8].life += temp/2;
+					parts[i].life -= temp/2;
+				}
+			}
+		}
+	}
 	return 0;
 }
