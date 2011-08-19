453d212f52a037cb10fd9b7f0f168e527439f19e
jacksonmj <mj-pt@jacksonmj.co.uk>
2011-08-19 15:00:48 +0100

    Memory leaks and uninitialised value in catalogue_ui

--- src/interface.c
+++ src/interface.c
@@ -5467,7 +5467,7 @@ void catalogue_ui(pixel * vid_buf)
 	int listy = 0, listxc;
 	int listx = 0, listyc;
 	pixel * vid_buf2;
-	float scrollvel, offsetf = 0.0f;
+	float scrollvel = 0.0f, offsetf = 0.0f;
 	char savetext[128] = "";
 	char * last = mystrdup("");
 	savelist_e *saves, *cssave, *csave;
@@ -5684,6 +5684,8 @@ openfin:
 
 	if(saves)
 		free_saveslist(saves);
+	free(last);
+	free(vid_buf2);
 	return;
 }
 
