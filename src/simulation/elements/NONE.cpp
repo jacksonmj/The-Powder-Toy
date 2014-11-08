3ced21c561adb10d54c26732ad76e661245ea721
jacksonmj <mj-pt@jacksonmj.co.uk>
2014-11-08 00:42:35 +0000

    TPT++ jacob1 76ce2de1: change some wall descriptions a little

--- src/simulation/elements/NONE.cpp
+++ src/simulation/elements/NONE.cpp
@@ -41,8 +41,8 @@ void NONE_init_element(ELEMENT_INIT_FUNC_ARGS)
 
 	elem->Weight = 100;
 
-	elem->DefaultProperties.temp = R_TEMP+0.0f	+273.15f;
-	elem->HeatConduct = 251;
+	elem->DefaultProperties.temp = R_TEMP+273.15f;
+	elem->HeatConduct = 0;
 	elem->Latent = 0;
 	elem->Description = "Erases particles.";
 
