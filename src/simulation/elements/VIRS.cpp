13d8d65fdb0ce9526458a298a25a2dbcbeb7da42
jacksonmj <mj-pt@jacksonmj.co.uk>
2014-11-16 15:37:16 +0000

    TPT++ jacob1,mniip: latest (2014-11-15) version of VIRS, VRSS, VRSG

---
+++ src/simulation/elements/VIRS.cpp
@@ -0,0 +1,176 @@
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "simulation/ElementsCommon.h"
+
+int VIRS_update(UPDATE_FUNC_ARGS)
+{
+	//pavg[0] measures how many frames until it is cured (0 if still actively spreading and not being cured)
+	//pavg[1] measures how many frames until it dies
+	int r, rx, ry, rndstore = rand();
+	if (parts[i].pavg[0])
+	{
+		parts[i].pavg[0] -= (rndstore&0x1) ? 0:1;
+		//has been cured, so change back into the original element
+		if (!parts[i].pavg[0])
+		{
+			sim->part_change_type(i,x,y,parts[i].tmp2);
+			parts[i].tmp2 = 0;
+			parts[i].pavg[0] = 0;
+			parts[i].pavg[1] = 0;
+			return 0;
+		}
+	}
+	//decrease pavg[1] so it slowly dies
+	if (parts[i].pavg[1] > 0)
+	{
+		if (((rndstore>>1)&0xD) < 1)
+		{
+			parts[i].pavg[1]--;
+			//if pavg[1] is now 0 and it's not in the process of being cured, kill it
+			if (!parts[i].pavg[1] && !parts[i].pavg[0])
+			{
+				sim->part_kill(i);
+				return 1;
+			}
+		}
+	}
+
+	//none of the things in the below loop happen while virus is being cured
+	if (parts[i].pavg[0])
+		return 0;
+
+	int rcount, ri, rnext, rt;
+	for (rx=-1; rx<2; rx++)
+	{
+		//reset rndstore, one random can last through 3 locations and reduce rand() calling by up to 6x as much
+		rndstore = rand();
+		for (ry=-1; ry<2; ry++)
+		{
+			if (x+rx>=0 && y+ry>=0 && x+rx<XRES && y+ry<YRES && (rx || ry))
+			{
+				FOR_PMAP_POSITION(sim, x+rx, y+ry, rcount, ri, rnext)
+				{
+					rt = parts[ri].type;
+					//spread "being cured" state
+					if ((rt == PT_VIRS || rt == PT_VRSS || rt == PT_VRSG) && parts[ri].pavg[0])
+					{
+						parts[i].pavg[0] = parts[ri].pavg[0] + (((rndstore&0x7)>>1) ? 2:1);
+						rndstore = rndstore >> 3;
+						return 0;
+					}
+					//soap cures virus
+					else if (rt == PT_SOAP)
+					{
+						parts[i].pavg[0] += 10;
+						if (!((rndstore&0x7)>>1))
+							sim->part_kill(ri);
+						return 0;
+					}
+					else if (rt == PT_PLSM)
+					{
+						if (surround_space && 10 + (int)(pv[(y+ry)/CELL][(x+rx)/CELL]) > (rand()%100))
+						{
+							sim->part_create(i, x, y, PT_PLSM);
+							return 1;
+						}
+					}
+					//transforms things into virus here
+					else if (rt != PT_VIRS && rt != PT_VRSS && rt != PT_VRSG && rt != PT_DMND)
+					{
+						if (!((rndstore&0xF)>>1))
+						{
+							parts[ri].tmp2 = rt;
+							parts[ri].pavg[0] = 0;
+							if (parts[i].pavg[1])
+								parts[ri].pavg[1] = parts[i].pavg[1] + ((rndstore>>4) ? 1:0);
+							else
+								parts[ri].pavg[1] = 0;
+							if (parts[ri].temp < 305.0f)
+								sim->part_change_type(ri, x+rx, y+ry, PT_VRSS);
+							else if (parts[ri].temp > 673.0f)
+								sim->part_change_type(ri, x+rx, y+ry, PT_VRSG);
+							else
+								sim->part_change_type(ri, x+rx, y+ry, PT_VIRS);
+						}
+						rndstore = rndstore >> 5;
+					}
+					//protons make VIRS last forever
+					else if (rt == PT_PROT)
+					{
+						parts[i].pavg[1] = 0;
+					}
+				}
+			}
+		}
+	}
+	return 0;
+}
+
+int VIRS_graphics(GRAPHICS_FUNC_ARGS)
+{
+	*pixel_mode |= PMODE_BLUR;
+	*pixel_mode |= NO_DECO;
+	return 1;
+}
+
+void VIRS_init_element(ELEMENT_INIT_FUNC_ARGS)
+{
+	elem->Identifier = "DEFAULT_PT_VIRS";
+	elem->Name = "VIRS";
+	elem->Colour = COLPACK(0xFE11F6);
+	elem->MenuVisible = 1;
+	elem->MenuSection = SC_LIQUID;
+	elem->Enabled = 1;
+
+	elem->Advection = 0.6f;
+	elem->AirDrag = 0.01f * CFDS;
+	elem->AirLoss = 0.98f;
+	elem->Loss = 0.95f;
+	elem->Collision = 0.0f;
+	elem->Gravity = 0.1f;
+	elem->Diffusion = 0.00f;
+	elem->PressureAdd_NoAmbHeat = 0.000f	* CFDS;
+	elem->Falldown = 2;
+
+	elem->Flammable = 0;
+	elem->Explosive = 0;
+	elem->Meltable = 0;
+	elem->Hardness = 20;
+
+	elem->Weight = 31;
+
+	elem->DefaultProperties.temp = 72.0f	+ 273.15f;
+	elem->HeatConduct = 251;
+	elem->Latent = 0;
+	elem->Description = "Virus. Turns everything it touches into virus.";
+
+	elem->State = ST_LIQUID;
+	elem->Properties = TYPE_LIQUID|PROP_DEADLY;
+
+	elem->LowPressureTransitionThreshold = IPL;
+	elem->LowPressureTransitionElement = NT;
+	elem->HighPressureTransitionThreshold = IPH;
+	elem->HighPressureTransitionElement = NT;
+	elem->LowTemperatureTransitionThreshold = 305.0f;
+	elem->LowTemperatureTransitionElement = PT_VRSS;
+	elem->HighTemperatureTransitionThreshold = 673.0f;
+	elem->HighTemperatureTransitionElement = PT_VRSG;
+
+	elem->DefaultProperties.pavg[1] = 250;
+
+	elem->Update = &VIRS_update;
+	elem->Graphics = &VIRS_graphics;
+}
---
+++ src/simulation/elements/VRSG.cpp
@@ -0,0 +1,78 @@
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "simulation/ElementsCommon.h"
+#include "simulation/elements/VIRS.h"
+
+int VRSG_graphics(GRAPHICS_FUNC_ARGS)
+{
+	*pixel_mode &= ~PMODE;
+	*pixel_mode |= FIRE_BLEND;
+	*firer = *colr/2;
+	*fireg = *colg/2;
+	*fireb = *colb/2;
+	*firea = 125;
+	*pixel_mode |= NO_DECO;
+	return 1;
+}
+
+void VRSG_init_element(ELEMENT_INIT_FUNC_ARGS)
+{
+	elem->Identifier = "DEFAULT_PT_VRSG";
+	elem->Name = "VRSG";
+	elem->Colour = COLPACK(0xFE68FE);
+	elem->MenuVisible = 0;
+	elem->MenuSection = SC_GAS;
+	elem->Enabled = 1;
+
+	elem->Advection = 1.0f;
+	elem->AirDrag = 0.01f * CFDS;
+	elem->AirLoss = 0.99f;
+	elem->Loss = 0.30f;
+	elem->Collision = -0.1f;
+	elem->Gravity = 0.0f;
+	elem->Diffusion = 0.75f;
+	elem->PressureAdd_NoAmbHeat = 0.000f	* CFDS;
+	elem->Falldown = 0;
+
+	elem->Flammable = 500;
+	elem->Explosive = 0;
+	elem->Meltable = 0;
+	elem->Hardness = 0;
+
+	elem->Weight = 1;
+
+	elem->DefaultProperties.temp = 522.0f + 273.15f;
+	elem->HeatConduct = 251;
+	elem->Latent = 0;
+	elem->Description = "Gas Virus. Turns everything it touches into virus.";
+
+	elem->State = ST_GAS;
+	elem->Properties = TYPE_GAS|PROP_DEADLY;
+
+	elem->LowPressureTransitionThreshold = IPL;
+	elem->LowPressureTransitionElement = NT;
+	elem->HighPressureTransitionThreshold = IPH;
+	elem->HighPressureTransitionElement = NT;
+	elem->LowTemperatureTransitionThreshold = 673.0f;
+	elem->LowTemperatureTransitionElement = PT_VIRS;
+	elem->HighTemperatureTransitionThreshold = ITH;
+	elem->HighTemperatureTransitionElement = NT;
+
+	elem->DefaultProperties.pavg[1] = 250;
+
+	elem->Update = &VIRS_update;
+	elem->Graphics = &VRSG_graphics;
+}
